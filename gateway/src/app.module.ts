44862ca3c7dce60c07361757c9d91753da6e5c99
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-06-28 14:24:54 +0300

    feature: added prisma & .env.sample

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
